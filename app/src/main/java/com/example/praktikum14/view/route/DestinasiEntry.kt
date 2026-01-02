36448cb39217fc6dc62d971464c61e0cb905a82d
kiieeaa <kiyaadinda5@gmail.com>
2026-01-02 16:35:51 +0700

    Objek yang mendefinisikan rute navigasi menuju halaman input/entry data siswa

---
+++ app/src/main/java/com/example/praktikum14/view/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+package com.example.praktikum14.view.route
+import com.example.praktikum14.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
