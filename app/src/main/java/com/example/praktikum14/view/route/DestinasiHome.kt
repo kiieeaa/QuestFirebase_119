48d0fcfbe69c9e5fbd7b8dfc21eab9892cb8e165
kiieeaa <kiyaadinda5@gmail.com>
2026-01-02 16:36:27 +0700

    Objek yang menentukan rute utama (Home) aplikasi untuk menampilkan daftar atau halaman utama siswa

---
+++ app/src/main/java/com/example/praktikum14/view/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.praktikum14.view.route
+import com.example.praktikum14.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
