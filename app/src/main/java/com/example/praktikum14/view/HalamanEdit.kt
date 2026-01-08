2d540591ed18cdc541866f6e2f1f9cdca798e771
kiieeaa <kiyaadinda5@gmail.com>
2026-01-08 19:42:23 +0700

    EditSiswaScreen merupakan halaman untuk mengubah (edit) data siswa yang sudah ada

---
+++ app/src/main/java/com/example/praktikum14/view/HalamanEdit.kt
@@ -0,0 +1,28 @@
+package com.example.praktikum14.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum14.view.route.DestinasiEdit
+import com.example.praktikum14.viewmodel.EditViewModel
+import com.example.praktikum14.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
