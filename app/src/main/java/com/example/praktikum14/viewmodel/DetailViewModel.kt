f4da1e911db94f3ec201c299ad7c8989847b552a
kiieeaa <kiyaadinda5@gmail.com>
2026-01-08 19:36:23 +0700

    DetailUiState merepresentasikan kondisi UI pada halaman detail siswa

---
+++ app/src/main/java/com/example/praktikum14/viewmodel/DetailViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.praktikum14.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum14.modeldata.Siswa
+import com.example.praktikum14.repositori.RepositorySiswa
+import com.example.praktikum14.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+
+sealed interface DetailUiState {
+    data class Success(val siswa: Siswa) : DetailUiState
+    object Error : DetailUiState
+    object Loading : DetailUiState
+}
